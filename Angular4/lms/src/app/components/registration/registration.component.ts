f8ba9c2fa9a4efc12c97689295ae5fbfbbddf80e
NehaDawda <nehadawra85@gmail.com>
2019-01-10 11:16:24 +0530

    10-Jan

--- Angular4/lms/src/app/components/registration/registration.component.ts
+++ Angular4/lms/src/app/components/registration/registration.component.ts
@@ -10,18 +10,23 @@ import { Property } from 'src/app/models/property.model';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  NewProspect: Prospect[];
+  NewProspect: Prospect;
+  NewProperty: Property;
   constructor(private _prospectlistservice: ProspectService) { }
 
   ngOnInit() {
   }
 
+  showSubElements(){
+    var loan_type = document.getElementById("loanType").value;
+    if(loan_type==="HOME_LOAN"){
+        document.getElementById("propertyFields").style.display = "block";
+    }
+}
   addProspect(prospectform: NgForm) {
-    console.log('Hello');
-
     
-    let newprospect= {
-      //prospectId: "R869",
+    this.NewProspect= {
+      prospectId: "R869",
       firstName: prospectform.value.firstName,
       lastName: prospectform.value.lastName,
       address: prospectform.value.address,
@@ -32,16 +37,16 @@ export class RegistrationComponent implements OnInit {
       aadharNumber: prospectform.value.aadharNumber,
       contactNumber: prospectform.value.contactNumber,
       email: prospectform.value.email,
-      //enquiryDate: new Date('12/09/2018'),
+      enquiryDate: null,
       dateOfBirth: prospectform.value.dateOfBirth,
       city: prospectform.value.city,
-      //creditLimit: prospectform.value.creditLimit,
-      //lastUpdatedCreditDate: new Date(''),
+      creditLimit: null,
+      lastUpdatedCreditDate: null,
       applicationStatus: "NEW",
       prospectAssetId: "122"
     };
 
-    let newproperty  = {
+    this.NewProperty  = {
       propertyId: "Property1",
     apartmentNo:"Property1",
     apartmentName:"Property1",
@@ -52,11 +57,11 @@ export class RegistrationComponent implements OnInit {
     state:"Property1",
     country:"Property1",
     zipCode:"Property1",
-    purchasedPrice:"5000000"
+    purchasedPrice:5000000
     }
 
-    console.log(newprospect)
-    this._prospectlistservice.addNewProspect(newprospect, newproperty).subscribe(prospect => { this.NewProspect });
+    console.log(this.NewProspect)
+    this._prospectlistservice.addNewProspect(this.NewProspect, this.NewProperty).subscribe(prospect => { this.NewProspect });
   }
 
 }
